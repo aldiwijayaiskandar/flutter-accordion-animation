56c9475cff191a20c120db4fabeb2e915cfa9d06
aldi <iskandar.aldi88@gmail.com>
2021-10-28 23:59:48 +0700

    adding animation

--- sharing_knowledge_animation/lib/main.dart
+++ sharing_knowledge_animation/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(MyApp());
@@ -9,32 +10,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double _accordionHeight = 75.00;
+  bool _open = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+          "Accordion Animation",
+          style: TextStyle(
+            color: Colors.blue,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
       ),
       body: Center(
-        child: Container(),
+        child: GestureDetector(
+          onTap: () => {
+            setState(() {
+              _open = !_open;
+              _accordionHeight = _open ? 150 : 75;
+            })
+          },
+          child: AnimatedContainer(
+            margin: EdgeInsets.only(left: 30, top: 100, right: 30, bottom: 50),
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            duration: Duration(milliseconds: 250),
+            height: _accordionHeight,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            alignment: Alignment.topLeft,
+            child: Container(
+              height: 75,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("This is an accordion"),
+                  Transform.rotate(
+                    angle: (_open ? 270 : 90) * math.pi / 180,
+                    child: Icon(
+                      Icons.chevron_right,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
